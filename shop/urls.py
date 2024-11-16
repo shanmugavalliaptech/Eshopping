fe22d4b98f2d2284bab4e35986ccd3a3a640d2ca
unknown <Administrator@APTDNS.COM>
2024-11-16 11:45:21 +0530

    first commit

---
+++ shop/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+ 
+urlpatterns= [
+    path('',views.home,name="home"),
+    path('register',views.register,name="register"),
+    path('login',views.login_page,name="login"),
+    path('logout',views.logout_page,name="logout"),
+    path('cart',views.cart_page,name="cart"),
+    path('favviewpage',views.favviewpage,name="favviewpage"),
+    path('collections',views.collections,name="collections"),
+    path('collections/<str:name>',views.collectionsview,name="collections"),
+    path('collections/<str:cname>/<str:pname>',views.product_details,name="product_details"),
+]
---
+++ shop/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import redirect, render
+from shop.forms import CustomUserForm
+from . models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+
+def home(request):
+  products=Product.objects.filter(trending=1)
+  return render(request,"shop/index.html",{"products":products})
+ 
+def logout_page(request):
+  if request.user.is_authenticated:
+    logout(request)
+    messages.success(request,"Logged out Successfully")
+  return redirect("/")
+ 
+ 
+def login_page(request):
+  if request.user.is_authenticated:
+    return redirect("/")
+  else:
+    if request.method=='POST':
+      name=request.POST.get('username')
+      pwd=request.POST.get('password')
+      user=authenticate(request,username=name,password=pwd)
+      if user is not None:
+        login(request,user)
+        messages.success(request,"Logged in Successfully")
+        return redirect("/")
+      else:
+        messages.error(request,"Invalid User Name or Password")
+        return redirect("/login")
+    return render(request,"shop/login.html")
+ 
+def register(request):
+  form=CustomUserForm()
+  if request.method=='POST':
+    form=CustomUserForm(request.POST)
+    if form.is_valid():
+      form.save()
+      messages.success(request,"Registration Success You can Login Now..!")
+      return redirect('/login')
+  return render(request,"shop/register.html",{'form':form})
+ 
+ 
+def collections(request):
+  catagory=Catagory.objects.filter(status=0)
+  return render(request,"shop/collections.html",{"catagory":catagory})
+ 
+def collectionsview(request,name):
+  if(Catagory.objects.filter(name=name,status=0)):
+      products=Product.objects.filter(category__name=name)
+      return render(request,"shop/products/index.html",{"products":products,"category_name":name})
+  else:
+    messages.warning(request,"No Such Catagory Found")
+    return redirect('collections')
+ 
+ 
+def product_details(request,cname,pname):
+    if(Catagory.objects.filter(name=cname,status=0)):
+      if(Product.objects.filter(name=pname,status=0)):
+        products=Product.objects.filter(name=pname,status=0).first()
+        return render(request,"shop/products/product_details.html",{"products":products})
+      else:
+        messages.error(request,"No Such Produtct Found")
+        return redirect('collections')
+    else:
+      messages.error(request,"No Such Catagory Found")
+      return redirect('collections')
+    
+def favviewpage(request):
+  if request.user.is_authenticated:
+    fav=Favourite.objects.filter(user=request.user)
+    return render(request,"shop/fav.html",{"fav":fav})
+  else:
+    return redirect("/")
+ 
+ 
+def cart_page(request):
+  if request.user.is_authenticated:
+    cart=Cart.objects.filter(user=request.user)
+    return render(request,"shop/cart.html",{"cart":cart})
+  else:
+    return redirect("/")
